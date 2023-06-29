2c7fdae4065a610c864bbdf6d1ef558adbf31bdd
h-babazono <babazono.hiroshi@da-vinci-studio.com>
2023-06-29 18:46:11 +0900

    update default system prompt

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
-  "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
+  "あなたは優秀なアシスタントですユーザーからの質問に丁寧に答えてください。";
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
